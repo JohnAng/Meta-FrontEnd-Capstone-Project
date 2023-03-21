0b09a9a3632d401afd7202d3bdf29299ada3ae29
Ioannis Angelakos <angelakos.john@gmail.com>
2023-03-21 21:20:32 +0200

    finished

--- web/Meta Front-end dev/React Basics/graded assessment/reactlab/src/App.js
+++ web/Meta Front-end dev/React Basics/graded assessment/reactlab/src/App.js
@@ -13,22 +13,32 @@ function App() {
 
 	function minus(e) {
 		// Add the code for the minus function
+		e.preventDefault();
+		setResult((result) => result - Number(inputRef.current.value));
 	}
 
 	function times(e) {
 		// Add the code for the plus function
+		e.preventDefault();
+		setResult((result) => result * Number(inputRef.current.value));
 	}
 
 	function divide(e) {
 		// Add the code for the divide function
+		e.preventDefault();
+		setResult((result) => result / Number(inputRef.current.value));
 	}
 
 	function resetInput(e) {
 		// Add the code for the resetInput function
+		e.preventDefault();
+		inputRef.current.value = 0;
 	}
 
 	function resetResult(e) {
 		// Add the code for the resetResult function
+		e.preventDefault();
+		setResult((result) => 0);
 	}
 
 	return (
@@ -37,9 +47,7 @@ function App() {
 				<h1>Simplest Working Calculator</h1>
 			</div>
 			<form>
-				<p ref={resultRef}>
-					{/* add the value of the current total */}
-				</p>
+				<p ref={resultRef}>{result}</p>
 				<input
 					pattern="[0-9]"
 					ref={inputRef}
@@ -48,10 +56,15 @@ function App() {
 				/>
 				<button onClick={plus}>add</button>
 				{/* Add the subtract button */}
+				<button onClick={minus}>subtract</button>
 				{/* Add the multiply button */}
+				<button onClick={times}>multiply</button>
 				{/* Add the divide button */}
+				<button onClick={divide}>divide</button>
 				{/* Add the resetInput button */}
+				<button onClick={resetInput}>reset input</button>
 				{/* Add the resetResult button */}
+				<button onClick={resetResult}>reset result</button>
 			</form>
 		</div>
 	);
