5365171ce3ed2de2152a6fa1780ba74c0563bd94
Ioannis Angelakos <angelakos.john@gmail.com>
2023-03-22 14:32:53 +0200

    update

--- web/Meta Front-end dev/Advanced React/lab-1/src/DessertsList.js
+++ web/Meta Front-end dev/Advanced React/lab-1/src/DessertsList.js
@@ -1,23 +1,19 @@
 function DessertsList(props) {
 	// Implement the component here.
-	const listDesserts = props.data
+	const lowCaloriesDesserts = props.data
 		.filter((dessert) => {
-			const filterCondition = dessert.calories <= 500;
+			const filterCondition = dessert.calories < 500;
 			return filterCondition;
 		})
 		.sort((dessert1, dessert2) => {
 			return dessert1.calories - dessert2.calories;
 		})
 		.map((dessert) => {
-			const itemText = `${dessert.name} - ${dessert.calories} cal`;
-			return <li key={dessert.name}>{itemText}</li>;
+			const dessertFormat = `${dessert.name} - ${dessert.calories} cal`;
+			return <li key={dessert.name}>{dessertFormat}</li>;
 		});
 
-	return (
-		<ul>
-			<li>{listDesserts}</li>
-		</ul>
-	);
+	return <ul>{lowCaloriesDesserts}</ul>;
 }
 
 export default DessertsList;
