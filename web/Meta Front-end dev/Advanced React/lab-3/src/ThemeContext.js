19491df2b0731e6e43402cfc2d58c1454607267a
Ioannis Angelakos <angelakos.john@gmail.com>
2023-03-25 20:06:10 +0200

    ignore

--- web/Meta Front-end dev/Advanced React/lab-3/src/ThemeContext.js
+++ web/Meta Front-end dev/Advanced React/lab-3/src/ThemeContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from "react";
 
-const ThemeContext = createContext("undefined");
+const ThemeContext = createContext(undefined);
 
 export const ThemeProvider = ({ children }) => {
 	const [theme, setTheme] = useState("light");
@@ -9,7 +9,8 @@ export const ThemeProvider = ({ children }) => {
 		<ThemeContext.Provider
 			value={{
 				theme,
-				toggleTheme: () => setTheme(theme === "light" ? "dark" : "light"),
+				toggleTheme: () =>
+					setTheme(theme === "light" ? "dark" : "light"),
 			}}
 		>
 			{children}
