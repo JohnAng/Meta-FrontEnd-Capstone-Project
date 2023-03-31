ff2e6168f8eed3fd5e68b02f2d22b8f2f307f737
Ioannis Angelakos <angelakos.john@gmail.com>
2023-03-31 16:38:14 +0300

    landing page

--- web/Meta Front-end dev/Advanced React/graded assesment/portfolio/src/components/LandingSection.js
+++ web/Meta Front-end dev/Advanced React/graded assesment/portfolio/src/components/LandingSection.js
@@ -1,22 +1,39 @@
-import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import React from "react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am John!";
 const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const bio2 = "specialized in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
-  <FullScreenSection
-    justifyContent="center"
-    alignItems="center"
-    isDarkBackground
-    backgroundColor="#2A4365"
-  >
+	<FullScreenSection
+		justifyContent="center"
+		alignItems="center"
+		isDarkBackground
+		backgroundColor="#2A4365"
+	>
+		<Avatar
+			size="2xl"
+			name="Ioannis Angelakos"
+			src="https://avatars.githubusercontent.com/u/11021994?s=400&u=4d94bd0eb38d6ed0a0b266b2e824e3e24806bc69&v=4"
+		/>
 
-  </FullScreenSection>
+		<VStack spacing={10}>
+			<p>{greeting}</p>
+			<Heading as="h1" size="2xl">
+				{bio1}
+			</Heading>
+		</VStack>
+
+		<VStack spacing={2}>
+			<Heading as="h2" size="2xl">
+				{bio2}
+			</Heading>
+		</VStack>
+	</FullScreenSection>
 );
 
 export default LandingSection;
