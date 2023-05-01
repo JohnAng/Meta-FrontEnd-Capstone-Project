bd67546325585dfe0043d8610eb2518360298eec
Ioannis Angelakos <angelakos.john@gmail.com>
2023-05-01 04:49:36 +0300

    hero done

--- web/Meta Front-end dev/Capstone Project/Little Lemon/little-lemon/src/components/Hero.js
+++ web/Meta Front-end dev/Capstone Project/Little Lemon/little-lemon/src/components/Hero.js
@@ -3,8 +3,8 @@ import heroImg from "../images/14.jpg";
 
 export default function Hero() {
 	return (
-		<section className="hero-container">
-			<div className="hero-container">
+		<section className="hero__section">
+			<div className="container">
 				<div className="hero-description">
 					<h1>Little Lemon</h1>
 					<h2>Chicago</h2>
@@ -12,11 +12,9 @@ export default function Hero() {
 						We are a family owned Mediterranean restaurant, focused
 						on traditional recipes served with a modern twist.
 					</p>
-					<button>Reserve a table</button>
+					<button className="btn">Reserve a table</button>
 				</div>
-				<div className="hero-image">
-					<img src={heroImg} alt="Brushcetta" />
-				</div>
+				<img src={heroImg} alt="serving food" className="hero__image" />
 			</div>
 		</section>
 	);
