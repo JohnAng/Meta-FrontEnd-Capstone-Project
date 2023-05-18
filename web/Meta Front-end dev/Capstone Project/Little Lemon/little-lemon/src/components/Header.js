258f3572007260b142ce3e77909676a2e7f19c1e
Ioannis Angelakos <angelakos.john@gmail.com>
2023-05-18 22:43:46 +0300

    finished home page

--- web/Meta Front-end dev/Capstone Project/Little Lemon/little-lemon/src/components/Header.js
+++ web/Meta Front-end dev/Capstone Project/Little Lemon/little-lemon/src/components/Header.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../images/logo.jpg";
 
 export default function Header() {
+	const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+	const handleMobileMenuToggle = () => {
+		setMobileMenuOpen(!isMobileMenuOpen);
+	};
+
 	return (
 		<>
 			<header className="header container">
@@ -10,7 +16,9 @@ export default function Header() {
 					<img src={logo} alt="Logo" className="header__logo" />
 				</NavLink>
 
-				<nav>
+				<nav
+					className={`header__nav ${isMobileMenuOpen ? "open" : ""}`}
+				>
 					<ul className="header__menu">
 						<li>
 							<NavLink className="header__link" to="/">
@@ -43,70 +51,90 @@ export default function Header() {
 							</NavLink>
 						</li>
 					</ul>
-					<button
-						aria-label="mobile nav button"
-						className="header__bars"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-						>
-							<path
-								fillRule="evenodd"
-								d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-								clipRule="evenodd"
-							/>
-						</svg>
-					</button>
 				</nav>
-			</header>
-			<div className="mobile-nav">
-				<NavLink to="/">
-					<img src={logo} alt="Logo" className="header__logo" />
-				</NavLink>
 
-				<nav>
-					<ul className="mobile-header__menu">
-						<li>
-							<NavLink className="mobile-nav__link" to="/">
-								Home
-							</NavLink>
-						</li>
-						<li>
-							<NavLink className="mobile-nav__link" to="/About">
-								About
-							</NavLink>
-						</li>
-						<li>
-							<NavLink className="mobile-nav__link" to="/Menu">
-								Menu
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								className="mobile-nav__link"
-								to="/Reservation"
-							>
-								Reservation
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								className="mobile-nav__link"
-								to="/OrderOnline"
-							>
-								Order Online
-							</NavLink>
-						</li>
-						<li>
-							<NavLink className="mobile-nav__link" to="/Login">
-								Login
-							</NavLink>
-						</li>
-					</ul>
-				</nav>
-			</div>
+				<button
+					aria-label="mobile nav button"
+					className={`header__bars ${isMobileMenuOpen ? "open" : ""}`}
+					onClick={handleMobileMenuToggle}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+					>
+						<path
+							fillRule="evenodd"
+							d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</button>
+			</header>
+
+			{isMobileMenuOpen && (
+				<div className="mobile-nav open">
+					<nav>
+						<ul className="mobile-header__menu">
+							<li>
+								<NavLink
+									className="mobile-nav__link"
+									to="/"
+									onClick={handleMobileMenuToggle}
+								>
+									Home
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									className="mobile-nav__link"
+									to="/About"
+									onClick={handleMobileMenuToggle}
+								>
+									About
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									className="mobile-nav__link"
+									to="/Menu"
+									onClick={handleMobileMenuToggle}
+								>
+									Menu
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									className="mobile-nav__link"
+									to="/Reservation"
+									onClick={handleMobileMenuToggle}
+								>
+									Reservation
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									className="mobile-nav__link"
+									to="/OrderOnline"
+									onClick={handleMobileMenuToggle}
+								>
+									Order Online
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									className="mobile-nav__link"
+									to="/Login"
+									onClick={handleMobileMenuToggle}
+								>
+									Login
+								</NavLink>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			)}
+
 			<div className="headerLine"></div>
 		</>
 	);
